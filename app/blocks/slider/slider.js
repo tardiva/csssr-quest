4dbab1a263dc17c75c6e462b2de85047135236ee
Irina <rannveig@mail.ru>
2016-10-20 13:25:12 +0300

    slider improvement

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -2,6 +2,7 @@
 
 	const bar = document.getElementById('bar');
 	const handle = document.getElementById('handle');
+	const input = document.getElementById('input');
 	const leftEdge = -5 /*handle.offsetWidth/2*/;
 	const rightEdge = bar.offsetWidth - 10 /*handle.offsetWidth/2*/;
 
@@ -13,7 +14,7 @@
 		let startPoint;
 		let endPoint;
 
-		if (event.type === 'touchmove') {eventCoord = event.touches[0].pageX;}
+		if (event.type === 'touchmove' || event.type === 'touchstart') {eventCoord = event.touches[0].pageX;}
 		else  {
 			    eventCoord = event.pageX;
 				offset = pageXOffset;
@@ -28,30 +29,52 @@
 		if (eventCoord > endPoint) {xCoord = rightEdge;}
 
 		handle.style.left = xCoord + 'px';
+		input.value = xCoord;
 	}
 
-	handle.addEventListener('mousedown', function () {
+	    handle.style.left = input.value + 'px';
+
+		handle.addEventListener('mousedown', function () {
 
 		let shift = event.pageX - handle.getBoundingClientRect().left - pageXOffset;
 
+		handle.style.transition = '';
 		handle.ondragstart = function () {
 			return false;
 		};
 
 		document.onmousemove = function () {
+			document.body.style.cursor = 'move';
+			bar.style.cursor = 'move';
 			moveHandle(event, shift);
 		};
 
 		document.onmouseup = function () {
+			document.body.style.cursor = 'default';
+			bar.style.cursor = 'pointer';
 			document.onmousemove = null;
 		};
 	});
 
 	handle.addEventListener('touchmove', function () {
 
-			event.preventDefault();
-			moveHandle(event, 0);
+		event.preventDefault();
+		handle.style.transition = '';
+		moveHandle(event, 0);
 
+	});
+
+	bar.addEventListener('click', function () {
+
+		let shift = 5/* handle.offsetWidth/2*/;
+		handle.style.transition = 'left .3s ease-out';
+		moveHandle(event, shift);
+	});
+
+	bar.addEventListener('touchstart', function () {
+
+		handle.style.transition = 'left .3s ease-out';
+		moveHandle(event, 0);
 	});
 
 })();
